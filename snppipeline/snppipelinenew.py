968d2a855586f7c45501bb5e046fde5ac512eb32
Hugh Rand <hugh.rand@fda.hhs.gov>
2014-03-10 14:21:20 -0400

    Yan changes to threading portion of code added to snppipelinenew.py.

--- snppipeline/snppipelinenew.py
+++ snppipeline/snppipelinenew.py
@@ -7,11 +7,10 @@ import os
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import subprocess
-import time
+import threading
 import imp
 utilsnew = imp.load_source('utilsnew', '/home/hugh.rand/projects/snppipeline/snppipeline/utilsnew.py')
-utils2 = imp.load_source('utils2', '/home/hugh.rand/projects/snppipeline/snppipeline/utils2.py')
-import threading
+
 
 class FuncThread(threading.Thread):
     def __init__(self,target,*args):
@@ -22,42 +21,21 @@ class FuncThread(threading.Thread):
     def run(self):
         self._target(*self._args)
 
-def pileup(file_path,snp_list_file_path,directory_name,opts):
-    seqString = ""
-    os.chdir(file_path)
+def pileup(filePath,snplistFilePath,dirName):
+    os.chdir(filePath)
+
     ####generate pileup files, using snplist file and the reference fasta file.
-    subprocess.call("samtools mpileup -l " + opts.mainPath + opts.snplistFileName + " -f " + opts.mainPath + opts.Reference + " reads.bam > reads.pileup",shell=True )
+    pileupFile = filePath + "/reads.pileup"
+    if os.path.isfile(pileupFile):
+        os.remove(pileupFile)
 
-    ####read in pileup file and store information to a hash
-    positionValueHash = utilsnew.create_consensus_dict(file_path + "/reads.pileup")
+    #subprocess.call("samtools mpileup -l " + opts.mainPath + opts.snplistFileName + " -f " + opts.mainPath + opts.Reference + " reads.bam > reads.pileup", shell=True)
+    os.system("samtools mpileup -l " + opts.mainPath + opts.snplistFileName + " -f " + opts.mainPath + opts.Reference + " " +opts.bamFileName +" > " + opts.pileupFileName)
 
-    ####append the nucleotide to the record
-    snplistFile_r = open(snp_list_file_path, "r")   #Conflict with other threads?
-    snplistFile_r.seek(0)             #Why is this necessary?
-    i = 0
-    while 1:
-        curSnplistLine = snplistFile_r.readline()
-        if not curSnplistLine:
-            break
-        i = i+1
-        curSnplistData = curSnplistLine.split()
-        if  len(curSnplistData) <2:
-            print "snplistfile: bad line# "+i+" line="+curSnplistLine
-            continue
-        chrom = curSnplistData[0]
-        pos = curSnplistData[1]
+    if not os.path.isfile(pileupFile):
+        print "pleup::tileup file not created: "+pileupFile
+    print "pileup function exit"
 
-        if positionValueHash.has_key(chrom + ":" + pos):
-            seqString += positionValueHash[chrom + ":" + pos]
-        else:
-            seqString += "-"
-    print "length of seqRecordString="+str(len(seqString))
-    seq = Seq(seqString)
-    seqRecord = SeqRecord(seq,id=directory_name)
-    records.append(seqRecord)
-    snplistFile_r.close()
-
-###read all *vcf file for SNP list
 #==============================================================================
 #     """Create SNP matrix
 #     
@@ -91,10 +69,8 @@ def pileup(file_path,snp_list_file_path,directory_name,opts):
 #     """
 #==============================================================================
 
-
-
 #### Command line usage
-usage = "usage: %prog -n 10 -d /home/yan.luo/Desktop/ -f path.txt -r reference -l snplist.txt -a snpma.fasta"
+usage = "usage: %prog -n 15 -d /home/yan.luo/Desktop/ -f path.txt -r reference -l snplist.txt -a snpma.fasta -b reads.bam -p reads.pileup"
 
 p = OptionParser(usage)
 p.add_option ("-n","--cpu",dest="maxThread",type="int",default=15,help="Max count of cocurrent thread (default=15)")
@@ -103,42 +79,48 @@ p.add_option ("-r","--Reference",dest="Reference",default="CFSAN001339_pacbio.fa
 p.add_option ("-f","--pathFileName",dest="pathFileName",default="path.txt",help="Path file name")
 p.add_option ("-l","--snplistFileName",dest="snplistFileName",default="snplist.txt",help="Snplist file name")
 p.add_option ("-a","--snpmaFileName",dest="snpmaFileName",default="snpma.fa",help="fasta file name")
+p.add_option ("-b","--bamFileName",dest="bamFileName",default="reads.bam",help="bam file name")
+p.add_option ("-p","--pileupFileName",dest="pileupFileName",default="reads.pileup",help="pileup file name")
 (opts,args)=p.parse_args()
 
-print("here it is: "+opts.mainPath + opts.pathFileName)
-sample_directories_list_file_object = open(opts.mainPath + opts.pathFileName, "r")
-snp_list_file_object = open(opts.mainPath + opts.snplistFileName, "w")
+
+pathFile = open(opts.mainPath + opts.pathFileName, "r")
+snplistFile = open(opts.mainPath + opts.snplistFileName, "w")
 snplistHash = dict()
 
+###read all *vcf file for SNP list
+
 while 1:
-    filePath = sample_directories_list_file_object.readline()[:-1]
+    filePath = pathFile.readline()[:-1]
     dirName = filePath.split(os.sep)[-1]
     if not filePath:
         break
-
-    vcf_file_object  = open(filePath + "/var.flt.vcf","r")
-    for curVcfFileLine in vcf_file_object:
-
+    print filePath
+    print dirName
+    vcfFile = open(filePath+ "/var.flt.vcf","r") 
+    while 1:
+        curVcfFileLine = vcfFile.readline()
+        if not curVcfFileLine:
+            break
         if curVcfFileLine.startswith("#"):
             continue
 
         curLineData = curVcfFileLine.split()
-        chrom       = curLineData[0]
-        pos         = curLineData[1]
-        info        = curLineData[7]
+        chrom = curLineData[0]
+        pos = curLineData[1]
+        info = curLineData[7]
         if str("INDEL") in curLineData[7]:
             continue
         infoFields = info.split(";")
-        dpFlag  = False
+        dpFlag = False
         af1Flag = False
         for infoField in infoFields:
             infoPair = infoField.split("=")
             if infoPair[0] == "DP" and int(infoPair[1]) >= 10:
                 dpFlag = True
-            elif ((infoPair[0] == "AF1" and infoPair[1] == "1" ) or
-                  (infoPair[0] == "AR"  and infoPair[1] == "1.00")):
+            elif (infoPair[0] == "AF1" and infoPair[1] == "1" ) or (infoPair[0] == "AR" and infoPair[1] == "1.00"):
                 af1Flag = True
-        # find a good record fo SNP position, save data to hash
+  # find a good record fo SNP position, save data to hash
         if dpFlag and af1Flag:
             if not snplistHash.has_key(chrom + "\t" + pos):
                 record = [1]
@@ -148,17 +130,18 @@ while 1:
                 record = snplistHash[chrom + "\t" + pos]
                 record[0] += 1
                 record.append(dirName)
-    vcf_file_object.close()
-
+    vcfFile.close()
+        
 for key in sorted(snplistHash.iterkeys()):
-    snp_list_file_object.write(key)
+    snplistFile.write(key)
     values = snplistHash[key]
     for value in values:
-        snp_list_file_object.write("\t" + str(value))
-    snp_list_file_object.write("\n")
-snp_list_file_object.close()
+        snplistFile.write("\t" + str(value))
+    snplistFile.write("\n")
+snplistFile.close()
 
-sample_directories_list_file_object.seek(0)
+
+pathFile.seek(0)
 snplistFilePath = opts.mainPath + opts.snplistFileName 
 fastaFile = open(opts.mainPath + opts.snpmaFileName, "w") 
 
@@ -166,22 +149,73 @@ records = []
 threads = []
 
 while 1:
-    filePath = sample_directories_list_file_object.readline()[:-1]
+    filePath = pathFile.readline()[:-1]
     dirName = filePath.split(os.sep)[-1]
-    print(filePath+"  "+dirName)
     if not filePath:
         break
 
-    t1 = FuncThread(pileup,filePath,snplistFilePath,dirName,opts)
-    threads.append(t1)
-    while threading.activeCount() > opts.maxThread:
-        time.sleep(15)
-    t1.start()
+    athread = FuncThread(pileup,filePath,snplistFilePath,dirName)
+    threads.append(athread)
+    athread.start()
 
-for thread in threads:          #Wait for threads to finish?
+    #print "current thread cnt="+str(threading.activeCount())
+    #while threading.activeCount() > opts.maxThread:
+    #    time.sleep(15)
+
+    cmdProcess = subprocess.Popen("ps -al | grep -c samtools", stdout=subprocess.PIPE, shell=True)
+    threadCnt = cmdProcess.communicate()[0]
+    print "current thread cnt="+threadCnt
+    #while int(threadCnt) > opts.maxThread:
+    #    time.sleep(15)
+    #    cmdProcess = subprocess.Popen("ps -al | grep -c samtools", stdout=subprocess.PIPE, shell=True)
+#	threadCnt = cmdProcess.communicate()[0]
+
+print "multi-thread loop end"
+
+for thread in threads:
     thread.join()
+
+print "all commands are finished"
+
+pathFile.seek(0)
+while 1:
+    filePath = pathFile.readline()[:-1]
+    dirName = filePath.split(os.sep)[-1]
+    if not filePath:
+        break
+
+    pileupFile = filePath + "/reads.pileup"
+    ###read in pileup file and store information to a hash
+    positionValueHash = utilsnew.create_consensus_dict(pileupFile)
+
+    ####append the nucleotide to the record
+    snplistFile_r = open(snplistFilePath, "r")
+    snplistFile_r.seek(0)
+    i = 0
+    seqString = ""
+    while 1:
+        curSnplistLine = snplistFile_r.readline()
+        if not curSnplistLine:
+            break
+        i = i+1
+        curSnplistData = curSnplistLine.split()
+        if  len(curSnplistData) <2:
+            print "snplistfile: bad line# "+i+" line="+curSnplistLine
+            continue
+        chrom = curSnplistData[0]
+        pos = curSnplistData[1]
+
+        if positionValueHash.has_key(chrom + ":" + pos):
+            seqString += positionValueHash[chrom + ":" + pos]
+        else:
+            seqString += "-"
+    #print "length of seqRecordString="+str(len(seqString))
+    seq = Seq(seqString)
+    seqRecord = SeqRecord(seq,id=dirName)
+    records.append(seqRecord)
+    snplistFile_r.close()
+
 
 ####write the records to fasta file           
 SeqIO.write(records, fastaFile, "fasta")
 fastaFile.close()
-print('Records written to fastaFile: '+opts.mainPath + opts.snpmaFileName)
