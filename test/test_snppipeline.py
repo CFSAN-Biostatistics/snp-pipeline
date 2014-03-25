bdf5c63a87130bcdf554acb6d6f16431abe1ac30
Hugh Rand <hugh.rand@fda.hhs.gov>
2014-03-25 07:42:54 -0400

    Lambda virus test set. Agona still needs work.

--- test/test_snppipeline.py
+++ test/test_snppipeline.py
@@ -5,6 +5,7 @@ import unittest
 import filecmp
 
 import imp
+#TODO fix these two lines
 snppipeline = imp.load_source('snppipeline', '/home/hugh.rand/projects/snppipeline/snppipeline/snppipeline.py')
 utils       = imp.load_source('utils', '/home/hugh.rand/projects/snppipeline/snppipeline/utils.py')
 
@@ -27,30 +28,30 @@ class Test(unittest.TestCase):
             'useOldPileups':False
         } 
         snppipeline.run_snp_pipeline(args_dict)
-        #TODO do diffs here
+        
         #Compare the files in the two directories whose names are given.
         #Returns three lists of file names: match, mismatch, errors.
-        directory_correct = '../test/codeComparisonFiles/testLambdaVirus' #TODO
-        directory_run_result = '../test/testLambdaVirus' #TODO
+        #TODO make path relative to where installed
+        directory_correct = '/home/hugh.rand/projects/snppipeline/test/codeComparisonFiles/testLambdaVirus' #TODO
+        directory_run_result = '/home/hugh.rand/projects/snppipeline/test/testLambdaVirus' #TODO
         files_to_compare = ['snplist.txt','snpma.fasta','sample1/reads.pileup',
                             'sample2/reads.pileup','sample3/reads.pileup',
-                            'sample4/reads.pileup','referenceSNP.fasta ']
+                            'sample4/reads.pileup','referenceSNP.fasta']
         match, mismatch, errors =  filecmp.cmpfiles(directory_correct,
                                                     directory_run_result,
                                                     files_to_compare,shallow=False)
-        print 'Match:', match
-        print 'Mismatch:', mismatch
-        print 'Errors:', errors
-                                    
-        self.assertEqual(True,match==7 and mismatch==0 and errors==0)
+        print('Match, Mismatch, Errors: '+str(len(match))+', '+str(len(mismatch))+', '+str(len(errors)))
+        self.assertEqual(True,len(match)    == len(files_to_compare) and
+                              len(mismatch) == 0 and
+                              len(errors)   == 0)
         pass
 
     def test_snppipeline_agona(self):
-        '''Run snppipeline with synthetic virus example.'''
+        '''Run snppipeline with agona 5 samples example.'''
         args_dict = {
-            'maxThread':3,      
-            'mainPath':'~/projects/snppipeline/test/testLambdaVirus/',        
-            'Reference':'reference.fasta',     
+            'maxThread':8,      
+            'mainPath':'~/projects/snppipeline/test/testAgonaMOM/',        
+            'Reference':'NC_011149.fasta',     
             'pathFileName':'path.txt',   
             'snplistFileName':'snplist.txt', 
             'snpmaFileName':'snpma.fa',
@@ -60,7 +61,29 @@ class Test(unittest.TestCase):
             'includeReference':True,
             'useOldPileups':False
         } 
-        #snppipeline.run_snp_pipeline(args_dict)
+        snppipeline.run_snp_pipeline(args_dict)
+        
+        #Compare the files in the two directories whose names are given.
+        #Returns three lists of file names: match, mismatch, errors.
+        #TODO make path relative to where installed
+        directory_correct = '/home/hugh.rand/projects/snppipeline/test/codeComparisonFiles/testAgonaMOM' #TODO
+        directory_run_result = '/home/hugh.rand/projects/snppipeline/test/testAgonaMOM' #TODO
+        files_to_compare = ['snplist.txt',
+                            'snpma.fasta',
+                            'samples/CFSAN_genomes/CFSAN000448/reads.pileup',  
+                            'samples/CFSAN_genomes/CFSAN000449/reads.pileup', 
+                            'samples/CFSAN_genomes/CFSAN000450/reads.pileup',
+                            'samples/SRA_data/ERR178930/reads.pileup',
+                            'samples/SRA_data/ERR178931/reads.pileup',
+                            'referenceSNP.fasta']
+        match, mismatch, errors =  filecmp.cmpfiles(directory_correct,
+                                                    directory_run_result,
+                                                    files_to_compare,shallow=False)
+        print('Match, Mismatch, Errors: '+str(len(match))+', '+str(len(mismatch))+', '+str(len(errors)))
+        
+        self.assertEqual(True,len(match)    == len(files_to_compare) and
+                              len(mismatch) == 0 and
+                              len(errors)   == 0)
         pass
 
 if __name__ == "__main__":
