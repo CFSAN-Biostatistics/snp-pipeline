eb8d595c99402800d164e213b9ef18baab47b4f8
Steve Davis <steven.davis@fda.hhs.gov>
2014-05-21 13:59:32 -0400

    Improve pep8 compliance. Remove trailing spaces. Add spaces after some commas. change some variable names.

--- snppipeline/utils.py
+++ snppipeline/utils.py
@@ -34,28 +34,28 @@ def pileup_wrapper(args):
 
 def pileup(sample_dir_path, options_dict):
     """Run samtools to generate pileup for one sample.
-    
+
     Description:
     Generate pileup file, using snplist file and the reference fasta file.
-    
+
     Args:
         sample_dir_path: Full path to directory for a sample.
         options_dict: Dictionary containing command-line options as per
             documention for run_snp_pipeline.
     """
-    
+
     os.chdir(sample_dir_path)
     verbose_print('Generating pileup file ' + options_dict['pileupFileName'] +
                   ' in '+sample_dir_path)
     pileup_file_path  = os.path.join(sample_dir_path, options_dict['pileupFileName'])
     snplist_file_path = (options_dict['mainPath'] +
                          options_dict['snplistFileName']) #TODO use join
-   
+
     #TODO - allow for use of previously done pileup via command line argument?
     if os.path.isfile(pileup_file_path):
         verbose_print('Removing old pileup file '+pileup_file_path)
         os.remove(pileup_file_path)
-    
+
     command_line = (
         'samtools mpileup ' +
             '-l ' + snplist_file_path +
@@ -69,7 +69,7 @@ def pileup(sample_dir_path, options_dict):
     #TODO review return values and clean up this next bit of code.
     #  see for details: https://docs.python.org/2/library/subprocess.html#replacing-os-system
     try:
-        return_code = subprocess.call(command_line,cwd=sample_dir_path,shell=True)
+        return_code = subprocess.call(command_line, cwd=sample_dir_path, shell=True)
         if return_code < 0:
             sys.stderr.write("Child was terminated by signal: " + str(return_code))
         else:
@@ -81,32 +81,32 @@ def pileup(sample_dir_path, options_dict):
     verbose_print("samtools return code: ", return_code)
     if not os.path.isfile(pileup_file_path):
         print('Pileup file not created: '+pileup_file_path)
-    
+
     verbose_print('pileup function exit')
-    
-    return(pileup_file_path)
+
+    return pileup_file_path
 
 def get_consensus_base_from_pileup(base, length, data):
     """Call the base for each SNP position
-    
+
     Description:
     Calls the base based on the pileup data for a SNP position with a given
-        length cutoff and with a given reference base.    
-    
+        length cutoff and with a given reference base.
+
     Args:
         base: Reference base.
         length: length cutoff.
         data: information from alignment in pileup format.
-        
+
     Returns:
         consensus_base: Consensus base from alignment.
-    
+
     Raises:
 
 TODO: all these examples return the value of the 1st argument, can we get some other examples?
 TODO: turn these into unit test cases
     Examples:
-    
+
     >>> print(get_consensus_base_from_pileup('T',10,',.....,,.,.,...,,,.,..A'))
     T
     >>> print(get_consensus_base_from_pileup('A',10,',.$.....,,.,.,...,,,.,..^+.'))
@@ -130,7 +130,7 @@ TODO: turn these into unit test cases
     consensus_base = ""
     #TODO consider using a Counter
     base_count_dict = {'.,': 0, 'A': 0, 'C': 0, 'G': 0, 'N': 0, 'T': 0}
-    
+
     #TODO move the char.upper out of the loop.
     #TODO move the set construction out of the loop, and we only need 4 elements in the set
     i = 0
@@ -139,11 +139,11 @@ TODO: turn these into unit test cases
         if char in sets.Set(['A','a','C','c','T','t','G','g','N','n']):
             base_count_dict[char.upper()] += 1
         elif char == '.' or char == ',':
-            base_count_dict['.,'] += 1      
+            base_count_dict['.,'] += 1
         elif char == '+' or char == '-':
             count_str = ""
             count = 1
-            while re.match("\d" , data[i + 1]):
+            while re.match("\d", data[i + 1]):
                 count_str += data[i + 1]
                 i += 1
             if count_str != "":
@@ -153,10 +153,10 @@ TODO: turn these into unit test cases
             if data[i + 1] != "." and data[i + 1] != ',':
                 i += 1
         i += 1
-            
+
     consensus_base = max(base_count_dict.iteritems(), key=operator.itemgetter(1))[0]
     if base_count_dict[consensus_base] <= (int(length)/2):
-        consensus_base =  "-" 
+        consensus_base = "-"
     elif consensus_base == ".,":
         consensus_base = base
 
@@ -165,16 +165,16 @@ TODO: turn these into unit test cases
 
 def create_consensus_dict(pileup_file_path):
     """Create a dict based on the information in a pileup file.
-    
+
     Given a path to a pileup file, create a dict based on the pileup
         data in that file. The dict contains the consensus
         base calls for positions in the reference sequence.
-    
+
     Args:
         pileup_file_path: full path to a pileup file.
-        
+
     Returns:
-        A dict mapping a key formed from a sequence and position to a 
+        A dict mapping a key formed from a sequence and position to a
         consensus sequence base. For example:
 
         {'gi|9626243|ref|NC_001416.1|:46842': 'G',
@@ -205,7 +205,7 @@ def create_consensus_dict(pileup_file_path):
     return position_value_dict
 
 
-def write_list_of_snps(file_path, snp_dict):    
+def write_list_of_snps(file_path, snp_dict):
     """Write out list of snps for all samples to a single file.
     """
     #TODO finish documentation
@@ -228,33 +228,33 @@ def write_reference_snp_file(reference_file_path, snp_list_file_path,
     #TODO actual code is more general than stated. Fix this.
 
     position_list = [line.split() for line in open(snp_list_file_path, "r")]
-    match_dict    = SeqIO.to_dict(SeqIO.parse(reference_file_path,"fasta"))
+    match_dict    = SeqIO.to_dict(SeqIO.parse(reference_file_path, "fasta"))
 
-    with open(snp_reference_file_path,"w") as snp_reference_file_object:
-        for orderedId in sorted(match_dict.keys()):
-            snp_reference_file_object.write(">" + orderedId + "\n")
+    with open(snp_reference_file_path, "w") as snp_reference_file_object:
+        for ordered_id in sorted(match_dict.keys()):
+            snp_reference_file_object.write(">" + ordered_id + "\n")
             for position in position_list:
-                ChromID, PosID = position[0:2]
-                if ChromID == orderedId:
-                    snp_reference_file_object.write(match_dict[orderedId][int(PosID)-1].upper())
-        
+                chrom_id, PosID = position[0:2]
+                if chrom_id == ordered_id:
+                    snp_reference_file_object.write(match_dict[ordered_id][int(PosID)-1].upper())
+
 
 def convert_vcf_files_to_snp_dict(list_of_sample_directories, options_dict):
     """convert list of vcf files to a single dict of quality SNPs.
-    
+
     We use several criteria from options_dict to evaluate SNPs.
- 
+
     Note use of get to cleanly handle case of missing key w/o exception.
     """
 
     snp_dict = dict()
-    
+
     for sample_directory in list_of_sample_directories:
 
         #TODO os.path.split(sample_directory) ???
-        sample_name  = sample_directory.split(os.sep)[-1]
+        sample_name = sample_directory.split(os.sep)[-1]
 
-        with open(os.path.join(sample_directory,"var.flt.vcf"), 'r') as vcf_file_object:
+        with open(os.path.join(sample_directory, "var.flt.vcf"), 'r') as vcf_file_object:
             vcf_reader = vcf.Reader(vcf_file_object)
             for vcf_data_line in vcf_reader:
                 verbose_print("vcf file data: ")
@@ -285,7 +285,6 @@ def convert_vcf_files_to_snp_dict(list_of_sample_directories, options_dict):
                         record = snp_dict[vcf_data_line.CHROM + "\t" + str(vcf_data_line.POS)]
                         record[0] += 1
                         record.append(sample_name)
-                        
-    return(snp_dict)
 
-    
+    return snp_dict
+
