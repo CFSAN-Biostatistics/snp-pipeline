a63e433b129619fbef161a63201ea8c1cddfce3b
Steve Davis <steven.davis@fda.hhs.gov>
2014-05-29 16:45:16 -0400

    Add scripts to the distribution.  Trying to add data files, but not working yet.

--- setup.py
+++ setup.py
@@ -3,19 +3,23 @@
 from setuptools import setup
 
 setup(
-    name='snppipeline',
+    name='snp-pipeline',
     version='0.1.1',
     description='Script and functions for SNP matrix construction',
     author='Yan Luo',
     author_email='yan.luo@fda.hhs.gov',
-    #url='http://', #TODO
+    url='https://github.com/CFSAN-Biostatistics/snp-pipeline',
     packages=['snppipeline','test'],
     long_description="""
-    snppipeline is a pipeline for the production of SNP matrices from 
+    snp-pipeline is a pipeline for the production of SNP matrices from 
     sequence data used in the phylogenetic analysis of pathogenic
     organisms sequenced from samples of interest to food safety.
     """,
+    
+    # download_url should be used if the distribution is not hosted on PyPI.
+    # GitHub can host the downloadable distribution if the repo is tagged
     #download_url='',#TODO    #TODO set this up properly
+    
     #TODO figure out how to next bit up
 #        exclude_package_data = {
 #            #Exclude any ARCHIVE directories
@@ -33,7 +37,14 @@ setup(
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering :: Bio-Informatics'
     ],
-    keywords='bioinformatics NGS',
+    scripts=[
+        'scripts/prepSamples.sh', 
+        'scripts/prepReference.sh',
+        'scripts/prepSequenceData.sh',
+        'scripts/runsnppipeline.py'
+    ],
+    package_data={'test' : ['testLambdaVirus/*.fa']},
+    keywords=['bioinformatics', 'NGS', 'SNP'],
     license='BSD',
     install_requires=[
         'biopython',
