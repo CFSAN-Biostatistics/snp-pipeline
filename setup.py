f64627858d2c820f9c810e4ff561a8fc33af2001
Steve Davis <steven.davis@fda.hhs.gov>
2014-05-30 09:07:39 -0400

    Modify setup.py and MANIFEST.in to include the test data.

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ setup(
     url='https://github.com/CFSAN-Biostatistics/snp-pipeline',
     packages=['snppipeline','test'],
     long_description="""
-    snp-pipeline is a pipeline for the production of SNP matrices from 
+    snp-pipeline is a pipeline for the production of SNP matrices from
     sequence data used in the phylogenetic analysis of pathogenic
     organisms sequenced from samples of interest to food safety.
     """,
-    
+
     # download_url should be used if the distribution is not hosted on PyPI.
     # GitHub can host the downloadable distribution if the repo is tagged
     #download_url='',#TODO    #TODO set this up properly
-    
+
     #TODO figure out how to next bit up
 #        exclude_package_data = {
 #            #Exclude any ARCHIVE directories
@@ -38,12 +38,21 @@ setup(
         'Topic :: Scientific/Engineering :: Bio-Informatics'
     ],
     scripts=[
-        'scripts/prepSamples.sh', 
+        'scripts/prepSamples.sh',
         'scripts/prepReference.sh',
         'scripts/prepSequenceData.sh',
         'scripts/runsnppipeline.py'
     ],
-    package_data={'test' : ['testLambdaVirus/*.fa']},
+
+    # Include the test data files listed below in the distribution.
+    # The package_data parameter only works for binary distributions.
+    # The same list of files is in MANIFEST.in for sdist distributions.
+    package_data={
+        'test' : ['testLambdaVirus/sample*/*.fq',
+                  'codeComparisonFiles/testLambdaVirus/*.fasta',
+                  'codeComparisonFiles/testLambdaVirus/sample*/*.pileup']
+    },
+
     keywords=['bioinformatics', 'NGS', 'SNP'],
     license='BSD',
     install_requires=[
