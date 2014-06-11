a43871ea877195e6de55ef2532ee8ba20cd5976b
Hugh Rand <hugh.rand@fda.hhs.gov>
2014-06-11 08:16:10 -0400

    More work done on running Agona data as test set. Not done yet.

--- scripts/runAgonaTest.sh
+++ scripts/runAgonaTest.sh
@@ -1,3 +1,4 @@
+#TODO DO NOT EXECUTE YET - NOT FINISHED
 #!/bin/bash
 #
 #Author: Hugh A. Rand (har)
@@ -22,57 +23,24 @@ fi
 BASEDIRECTORY=$1
 
 #Prep work
-#  Copy the sequence data to work on to a test directory.
-cd $BASEDIRECTORY'/test'
-cp -r testLambdaVirusClean testLambdaVirus
-cd testLambdaVirus
-ls -1 --color=never $PWD/samples > sampleDirectoryNames.txt
-
-#Set up reference sequence
-$BASEDIRECTORY'/scripts/prepReference.sh' lambda_virus
-
-#Set up sample sequence
-#  Note: This could be run in parallel using gnu parallel (workstation) or
-#    qsub (PBS on HPC)
-cat sampleDirectoryNames.txt | xargs -n 1 $BASEDIRECTORY'/scripts/prepSamples.sh' lambda_virus
-        
-#Run snp pipeline (samtools pileup in parallel and combine alignment and pileup to
-#   generate snp matrix)
-#TODO replace 'path.txt' with better name
-ls -d -1 --color=never $PWD/samples/* > path.txt
-#$BASEDIRECTORY'/scripts/runsnppipeline.py' -n 10 -d ~/mnt/biob/svn/Biostats/rand/snppipeline/test/testLambdaVirus/ -f path.txt -r reference/lambda_virus.fasta -l snplist.txt -a snpma.fasta -i True
-$BASEDIRECTORY/test/test_snppipeline.py -v
-
-
-
-
-#####################################################
-#Example: Set up to run the snp pipeline and run it.#
-#####################################################
-
-#Prep work
 cd $BASEDIRECTORY'/test'
 mkdir testAgona #directory to work in
 cd testAgona
-mkdir reference samples
 
-#Get sequence data
-#echo -e "SRR955145\nSRR955146\nERR178930\nERR178931\n" > sampleDirectoryNames.txt
+#Fetch sequence data
+#TODO set it so it only fetched the reference sequence once!!
 echo -e "ERR178926\nERR178927\nERR178928\nERR178929\nERR178930\n" > sampleDirectoryNames.txt
 cat sampleDirectoryNames.txt | xargs -n 1 $BASEDIRECTORY'/scripts/prepSequenceData.sh' NC_011149
 
-#More prep work
-ls -1 --color=never $PWD/samples > sampleDirectoryNames.txt
-
-#Set up reference sequence
+#Prep up reference sequence
 $BASEDIRECTORY'/scripts/prepReference.sh' NC_011149
 
-#Set up sample sequence
+#Prep sample sequence
 #  Note: This could be run in parallel using gnu parallel (workstation) or qsub (PBS on HPC)
 cat sampleDirectoryNames.txt | xargs -n 1 $BASEDIRECTORY'/snppipeline/scripts/prepSamples.sh' NC_011149
         
 #Run snp pipeline (samtools pileup in parallel and combine alignment and pileup to generate snp matrix)
 ls -d -1 --color=never $PWD/samples/* > path.txt
-$BASEDIRECTORY'/scripts/runsnppipeline.py' -n 10 -d $PWD -f path.txt -r reference/NC_011149.fasta -l snplist.txt -a snpma.fasta -i True
-
+#$BASEDIRECTORY'/scripts/runsnppipeline.py' -n 10 -d $PWD -f path.txt -r reference/NC_011149.fasta -l snplist.txt -a snpma.fasta -i True
+test/test_snppipeline.py -v
 
