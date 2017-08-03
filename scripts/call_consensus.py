8b2caffc746943ee0b5208393f8a8e0b5f604198
Steve Davis <steven.davis@fda.hhs.gov>
2017-08-03 11:21:19 -0400

    Add deprecation warnings to old scripts.

--- scripts/call_consensus.py
+++ scripts/call_consensus.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import subprocess
 import sys
+
+print("\nWarning: this script is deprecated and will be removed in a future SNP Pipeline release.", file=sys.stderr)
+print("Use cfsan_snp_pipeline.\n", file=sys.stderr)
 
 subprocess_args = ["cfsan_snp_pipeline", "call_consensus"] + sys.argv[1:]
 ret = subprocess.call(subprocess_args)
